3c915e20a61b8d72c5f9571998c130ccac17823f
Shrividya Ballapadavu <shrividya94@gmail.com>
2021-04-09 18:51:20 -0700

    Added excecute function and other routines

--- sql5300.cpp
+++ sql5300.cpp
@@ -1,5 +1,6 @@
 //
 // Created by jarak on 4/6/2021.
+// CPSC-5300 Butterfly ,Sprint Verano: Shrividya Ballapadavu, Jara Lindsay
 //
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,8 +8,8 @@
 #include <iostream>
 #include <algorithm>
 #include "sql5300.h"
-#include <sqlhelper.h>
-#include <SQLParser.h>
+#include "sqlhelper.h"
+#include "SQLParser.h"
 #include <db_cxx.h>
 #include <string>
 
@@ -18,35 +19,224 @@ const char *SQLSHELL = "sql5300.db";
 const unsigned int BLOCK_SZ = 4096;
 
 
-sql5300::sql5300() {
+std::string convertOperatorExpressionToString(hsql::Expr *expr,std::string res){
+	if (expr == NULL) {
+
+            return res;
+          }
+
+         switch (expr->opType) {
+		 case hsql::Expr::SIMPLE_OP:
+                         res+=std::string(" ")+(char)expr->opChar+" ";
+                         break;
+		 case hsql::Expr::AND:
+                         res+=std::string(" AND");
+                         break;
+                 case hsql::Expr::OR:
+                         res+=std::string(" OR");
+                         break;
+                 case hsql::Expr::NOT:
+                         res+=std::string(" NOT");
+                         break;
+                 default:
+                         res+=std::string(" ")+(char)expr->opType;
+                         break;
+           }
+	 res=convertExpressionToString(expr->expr,res);
+         if (expr->expr2 != NULL){
+		res=convertExpressionToSring(expr->expr2, res);
+          }
+	 return res;
+}
+
+std::string convertExpressionToString(hsql::Expr* expr,std::string res){
+
+	switch (expr->type) {
+		case hsql::kExprStar:
+			res+=" * ";
+			break;
+		case hsql::kExprColumnRef:
+		    res+=std::string(" ")+expr->name+",";
+		    break;
+    
+		case hsql::kExprLiteralFloat:
+			res+=std::string(" ")+(char)expr->fval+" ";
+			break;
+		case hsql::kExprLiteralInt;
+			res+=std::string(" ")+(char)expr->ival+" ";
+			break;
+		case hsql::kExprLiteralString:
+			res+=std::string(" ")+expr->name+" ";
+			break;
+                        /*
+	    case hsql::kExprTableColumnRef:
+            std::cout<<"kExprLiteralString";
+            res+=std::string(" ")+expr->table+expr->name;
+                         */
+		case hsql::kExprFunctionRef:
+			res+=std::string(" ")+expr->name+" "+expr->expr->name+" "; 
+			break;
+		case hsql::kExprOperator:
+		    res=convertOperatorExpressionToString(expr, res);
+		    break;
+		    default:
+		        fprintf(stderr, "Unrecognized expression type %d\n", expr->type);
+		        break;
+    }
+    if (expr->alias != NULL) {
+      res+=std::string(".")+expr->alias+" ";
+      
+    }
+    
+    return res;
+}
+
+std::string convertTableRefInfoToString(hsql::TableRef* table,std::string res){
+	switch (table->type) {
+		case hsql::kTableName:
+			res+=std::string(" ")+(char*)table->name+" ";
+                        break;
+                
+		case hsql::kTableSelect:
+                       // res=convertSelectStatementInfo(table->select, res);
+                        break;
+			
+		case hsql::kTableJoin:	
+			res=convertTableRefInfoToString(table->join->left,res);
+			res+=" LEFT ";
+			res+=" JOIN ";
+                        res=convertTableRefInfoToString(table->join->right,res);
+			res+=" ON ";
+                        res=convertExpressionToString(table->join->condition,res);
+
+                        break;
+		
+		case hsql::kTableCrossProduct:
+                        for (hsql::TableRef* tbl : *table->list)
+			       	res=convertTableRefInfoToString(tbl,res);
+                        break;
+    }
+    if (table->alias != NULL) {
+      res+=std::string("AS ")+(char*)table->alias+" ";
+    }
+
+    return res;
+}
+std::string convertSelectStatementInfo(const hsql::SelectStatement* stmt,std::string res){
+         res+=" SELECT";
+
+         for(hsql::Expr* expr : *stmt->selectList){
+		 res=convertExpressionToString(expr,res);
+	 }
+
+         res+=" FROM";
+         res=convertTableRefInfoToString(stmt->fromTable,res);
+         if(stmt->whereClause != NULL) {
+               res+=" WHERE ";
+               res=convertExpressionToString(stmt->whereClause, res);
+         }
+
+         return res;
 
 }
 
-//TODO: Fix shell.execute() by adding query parameter (AST type?)
-//TODO: Convert AST to string
-//SQLParserResult*
-std::string sql5300::execute() {
+
+
+/**
+ * Convert the hyrise ColumnDefinition AST back into the equivalent SQL
+ * @param col  column definition to unparse
+ * @return     SQL equivalent to *col
+ */
+std::string columnDefinitionToString(const hsql::ColumnDefinition *col) {
+    std::string ret(col->name);
+    switch(col->type) {
+	    case hsql::ColumnDefinition::DOUBLE:
+            ret += " DOUBLE";
+            break;
+            case hsql::ColumnDefinition::INT:
+            ret += " INT";
+            break;
+	    case hsql::ColumnDefinition::TEXT:
+            ret += " TEXT";
+            break;
+            default:
+            ret += " ...";
+            break;
+    }
+    return ret;
+}
+
+
+std::string convertCreateStatementInfo(const hsql::CreateStatement *stmt,std::string res){
+	res+="CREATE TABLE ";
+	res+=std::string("")+stmt->tableName;
+
+	
+        if(stmt->columns != NULL) {
+		res+=" (";
+		for (auto col_name:*stmt->columns) {
+			res+=std::string("")+columnDefinitionToString(col_name);
+			res+=",";
+                  
+                 
+             }
+		res+=")";
+        }
+        
+                   
+        
+	return res;
+}
+
+std::string execute(const hsql::SQLStatement* stmt,std::string res)  {
 
     // Test
     std::cout << "\nGot to execute\n" << std::endl;
+   // printf("Parsed successfully!\n");
+   // printf("Number of statements: %lu\n", result->size());
+    
+    
+    // Print a statement summary.
+    // hsql::printStatementInfo(result->getStatement(i));
+   // const hsql::SQLStatement* stmt=result->getStatement(i);
+       
+    switch (stmt->type()) {
+	    case hsql::kStmtSelect:
+		       res+=convertSelectStatementInfo((const hsql::SelectStatement*) stmt, res);
+                       break;
+	    case hsql::kStmtCreate:
+                       res+=convertCreateStatementInfo((const hsql::CreateStatement*) stmt, res);
+                       break;
+	      
+	   
+	      
+      }
+     
+       
+   
 
 
 
 
-
-    // Delete
-    std::string hello = "Hello";
-    return hello;
+  
+    return res;
 
 }
 
 int main( int argc, char *argv[]) {
 
-    sql5300 shell;
-    std::string query = argv[1];
-
+   // sql5300 shell;
+    
+   
     // TODO: check if this is correct for creating th db?
     //Create DB environment
+    /*
+    std::cout << "Have you created a dir: ~/" << HOME  << "? (y/n) " << std::endl;
+    std::string ans;
+    std::cin >> ans;
+    if( ans[0] != 'y')
+	return 1;
+    */
     const char *home = std::getenv("HOME");
     std::string envdir = std::string(home) + "/" + HOME;
 
@@ -60,14 +250,16 @@ int main( int argc, char *argv[]) {
     db.set_re_len(BLOCK_SZ);
     db.open(NULL, SQLSHELL, NULL, DB_RECNO, DB_CREATE | DB_TRUNCATE, 0644);
 
-
+    char newquery[200];
     // User prompt loop
     while (true) {
+        // Take in new query
+        std::cout << "SQL> ";
+        std::cin.getline(newquery,200);
+        std::string query = newquery;
+        std::cout << query<< std::endl;
 
-        //Parse response
-        hsql::SQLParserResult *result = hsql::SQLParser::parseSQLString(query);
-
-        // Convert query to lowercase for quit
+	// Convert query to lowercase for quit
         std::string lowercase = query;
         transform(lowercase.begin(), lowercase.end(), lowercase.begin(), ::tolower);
 
@@ -77,12 +269,26 @@ int main( int argc, char *argv[]) {
             break;
         }
 
+        //Parse response
+        hsql::SQLParserResult *result = hsql::SQLParser::parseSQLString(query);
+       
         //Valid response
         if (result->isValid()) {
             printf("Valid");
+	    std::string res;
             //TODO: Fix shell.execute() by adding query parameter (AST type?)
-            shell.execute();
+	    for (uint i = 0; i < result->size(); ++i) {
+		    res+=execute(result->getStatement(i),res);
+	    }
+	    
+            // Print a statement summary.
+           // hsql::printStatementInfo(result->getStatement(i));
+	   // res=execute(result,res);
+            
+            std::cout<<res<<std::endl;
+	    //std::string res=shell.execute(&result);
             delete result;
+	    
 
 
         } else { //Invalid response
@@ -90,14 +296,7 @@ int main( int argc, char *argv[]) {
             fprintf(stderr, "%s (L%d:%d)\n", result->errorMsg(),
                     result->errorLine(), result->errorColumn());
             delete result;
-        }
-
-        // Take in new query
-        std::cout << "SQL> ";
-        char newquery[200];
-        std::cin.getline(newquery,200);
-        query = newquery;
-        std::cout << newquery<< std::endl;
+        }      
 
     }
 
