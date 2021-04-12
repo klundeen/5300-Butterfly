ce4264e2ad7a19cf4edd661426e4ca184bb317a1
Lindsjar37 <55968751+Lindsjar37@users.noreply.github.com>
2021-04-12 10:19:24 -0700

    Added ast conversion methods to sql5300.h

--- sql5300.h
+++ sql5300.h
@@ -5,18 +5,38 @@
 #ifndef INC_5300_BUTTERFLY_SQL5300_H
 #define INC_5300_BUTTERFLY_SQL5300_H
 
+#include "sqlhelper.h"
+#include "SQLParser.h"
 
 class sql5300 {
 public:
 
     // Constructor
-    sql5300();
+    sql5300() = default;
 
-    // Execute function:
-    //  Takes in an AST and converts it to string and prints the response
-    // SQLParserResult*
-    //TODO: Fix execute() by adding query parameter (AST type?)
-    std::string execute();
+    // Destructor
+    ~sql5300();
+
+    // Convert the hyrise Expression AST back into the equivalent SQL String
+    std::string convertExpressionToString(hsql::Expr *expr, std::string res);
+
+    // Convert the hyrise SelectStatement AST back into the equivalent SQL
+    std::string convertSelectStatementInfo(const hsql::SelectStatement *stmt, std::string res);
+
+    // Convert the hyrise OperatorExpression AST back into the equivalent SQL String
+    std::string convertOperatorExpressionToString(hsql::Expr *expr, std::string res);
+
+    // Convert the hyrise TableRefInfo AST back into the equivalent SQL String
+    std::string convertTableRefInfoToString(hsql::TableRef *table, std::string res);
+
+    // Convert the hyrise ColumnDefinition AST back into the equivalent SQL
+    std::string columnDefinitionToString(const hsql::ColumnDefinition *col);
+
+    // Convert the hyrise CreateStatement AST back into the equivalent SQL
+    std::string convertCreateStatementInfo(const hsql::CreateStatement *stmt, std::string res);
+
+    // Execute function: Convert the hyrise SQLStatement AST back into the equivalent SQL
+    std::string execute(const hsql::SQLStatement *stmt, std::string res);
 
 };
 
